764693fc809f0231433d5bb5a7a7f62e1ffe0ddd
Solomon <aaasaasas0@hotmail.com>
2015-07-06 15:21:10 +0800

    add cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    # if session[:counter].nil?
+    #   @counter = 0
+    # else
+    #   @counter = session[:counter] + 1
+    # end
+    @counter = session[:counter].nil? ? 0 : session[:counter] + 1
+    session[:counter] = @counter
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 
+  has_many :line_itmes
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
   validates :title,length: {minimum: 10, message: "more than 10 words please."}
@@ -9,4 +11,18 @@ class Product < ActiveRecord::Base
                           with: %r{\.(gif|jpg|png)\z}i,
                           message: 'must be a URL for GIF, JPG or PNG image.'
                       }
+
+  private
+
+    # ensure that there are no line items referencing this product
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
