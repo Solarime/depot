6d3f2499eb3103cd41e2435ff63bfde127f3a655
Solomon <aaasaasas0@hotmail.com>
2015-07-07 10:25:13 +0800

    Task E

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,7 +26,7 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.add_product(product.id)
+    @line_item = @cart.add_product(product.id, product.price)
     session[:counter]=0
 
     respond_to do |format|
